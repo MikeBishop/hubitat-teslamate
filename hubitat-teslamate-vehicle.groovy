f9193929b765180ced0a40b35c2ed5bb23869724
Mike Bishop <mbishop@evequefou.be>
2022-04-15 02:18:28 -0400

    Settings to suppress chatty events

--- hubitat-teslamate-vehicle.groovy
+++ hubitat-teslamate-vehicle.groovy
@@ -6,43 +6,24 @@ import groovy.transform.Field
 
 metadata {
     definition(name: "TeslaMate Vehicle", namespace: "evequefou", author: "Mike Bishop", component: false) {
+// Always Available
         capability "PresenceSensor"
-        capability "PowerSource"
-        capability "VoltageMeasurement"
-        capability "CurrentMeter"
-        capability "PowerMeter"
+        attribute "geofence", "STRING"
         capability "Battery"
-        capability "TemperatureMeasurement"
-        capability "ContactSensor"
-        capability "Lock"
-
+        capability "PowerSource"
         attribute "state", "STRING"
         attribute "since", "DATE"
         attribute "healthy", "ENUM", ["true", "false"]
         attribute "version", "STRING"
         attribute "update_available", "ENUM", ["true", "false"]
         attribute "update_version", "STRING"
-        attribute "model", "STRING"
-        attribute "trim_badging", "STRING"
-        attribute "exterior_color", "STRING"
-        attribute "wheel_type", "STRING"
-        attribute "spoiler_type", "STRING"
-        attribute "geofence", "STRING"
-        attribute "latitude", "NUMBER"
-        attribute "longitude", "NUMBER"
-        attribute "shift_state", "STRING"
-        attribute "power", "NUMBER"
-        attribute "speed", "NUMBER"
-        attribute "heading", "NUMBER"
-        attribute "elevation", "NUMBER"
-        attribute "locked", "ENUM", ["true", "false"]
-        attribute "sentry_mode", "ENUM", ["true", "false"]
-        attribute "is_user_present", "ENUM", ["true", "false"]
-        attribute "is_climate_on", "ENUM", ["true", "false"]
-        attribute "inside_temp", "NUMBER"
-        attribute "outside_temp", "NUMBER"
-        attribute "is_preconditioning", "ENUM", ["true", "false"]
-        attribute "odometer", "NUMBER"
+        attribute "scheduled_charging_start_time", "DATE"
+        attribute "time_to_full_charge", "NUMBER"
+
+// Power and Charging Details
+        capability "VoltageMeasurement"
+        capability "CurrentMeter"
+        capability "PowerMeter"
         attribute "est_battery_range", "NUMBER"
         attribute "rated_battery_range", "NUMBER"
         attribute "ideal_battery_range", "NUMBER"
@@ -52,40 +33,37 @@ metadata {
         attribute "charge_port_door_open", "ENUM", ["true", "false"]
         attribute "charge_current_request", "NUMBER"
         attribute "charge_current_request_max", "NUMBER"
-        attribute "scheduled_charging_start_time", "DATE"
-        attribute "time_to_full_charge", "NUMBER"
+
+// Temperature
+        capability "TemperatureMeasurement"
+        attribute "is_climate_on", "ENUM", ["true", "false"]
+        attribute "inside_temp", "NUMBER"
+        attribute "outside_temp", "NUMBER"
+        attribute "is_preconditioning", "ENUM", ["true", "false"]
+
+// Security
+        capability "ContactSensor"
+        capability "Lock"
+        attribute "locked", "ENUM", ["true", "false"]
+        attribute "sentry_mode", "ENUM", ["true", "false"]
+
+// Coarse Location & Driving
+        attribute "shift_state", "STRING"
+        attribute "odometer", "NUMBER"
+
+// Detailed Location & Driving
+// WARNING:  These are exceptionally chatty;
+//    do not enable unless critical!
+// What you want to do can probably be done
+// by defining a geofence in TeslaMate instead.
+        attribute "latitude", "NUMBER"
+        attribute "longitude", "NUMBER"
+        attribute "speed", "NUMBER"
+        attribute "heading", "NUMBER"
+        attribute "elevation", "NUMBER"
+
     }
     preferences {
-        input(
-            name: "homeGeofence",
-            type: "string",
-            title: "Geofence for Presence",
-            required: false,
-            defaultValue: "Home"
-        )
-        input(
-            name: "temperatureIndoor",
-            type: "bool",
-            title: "Temperature reflects Indoor (On) or Outdoor (Off)",
-            required: false,
-            defaultValue: true
-        )
-        input(
-            name: "tempFormat",
-            type: "enum",
-            required: true,
-            defaultValue: "Fahrenheit (°F)",
-            title: "Display Unit - Temperature: Fahrenheit (°F) or Celsius (°C)",
-            options: ["Fahrenheit (°F)", "Celsius (°C)"]
-        )
-        input(
-            name: "rangeFormat",
-            type: "enum",
-            required: true,
-            defaultValue: "miles (mi)",
-            title: "Display Unit - Range: miles or kilometers",
-            options: ["miles (mi)", "kilometers (ki)"]
-        )
         input(
             name: "debugLogging",
             type: "bool",
@@ -98,13 +76,10 @@ metadata {
 
 void updated() {
     info("Updated...")
-    warn("description logging is: ${txtEnable == true}")
 }
 
 void installed() {
     info("Installed...")
-    device.updateSetting("txtEnable",[type:"bool",value:true])
-    refresh()
 }
 
 void parse(String description) { log.warn "parse(String description) not implemented" }
@@ -113,32 +88,11 @@ void lock() { log.warn "lock not implemented" }
 
 void unlock() { log.warn "unlock not implemented" }
 
-@Field final Map Transforms = [
-    battery_level: "battery",
-    charger_voltage: "voltage",
-    charger_actual_current: "current",
-    charger_phases: "phases",
-    locked: "lock",
-    est_battery_range_km: "est_battery_range",
-    rated_battery_range_km: "rated_battery_range",
-    ideal_battery_range_km: "ideal_battery_range"
-]
-
 void parse(List description) {
     description.each {
-        if( Transforms[it.name] ) {
-            it.name = Transforms[it.name]
-        }
         debug(it)
 
         switch(it.name) {
-            case "geofence":
-                sendEvent(it)
-                sendEvent([
-                    "name": "presence",
-                    "value": it.value == settings?.homeGeofence ? "present" : "not present"
-                ])
-                break
             case "plugged_in":
                 sendEvent([
                     "name": "powerSource",
@@ -157,27 +111,6 @@ void parse(List description) {
                     ])
                 }
                 break
-            case "lock":
-                sendEvent([
-                    "name": it.name,
-                    "value": it.value == "true" ? "locked" : "unlocked"
-                ])
-                break
-            case "inside_temp":
-            case "outside_temp":
-                it.value = Float.parseFloat(it.value)
-                if( settings?.tempFormat == "Fahrenheit (°F)" ) {
-                    // Convert temperature formats
-                    it.value = celsiusToFahrenheit(it.value)
-                }
-                if( settings?.temperatureIndoor ^ it.name == "outside_temp" ) {
-                    sendEvent([
-                        "name": "temperature",
-                        "value": it.value
-                    ])
-                }
-                sendEvent(it)
-                break
             case "windows_open":
             case "doors_open":
             case "frunk_open":
@@ -195,14 +128,6 @@ void parse(List description) {
                     "name": "contact",
                     "value": contact
                 ])
-                sendEvent(it)
-                break
-            case "est_battery_range":
-            case "rated_battery_range":
-            case "ideal_battery_range":
-                if( settings?.rangeFormat == "miles (mi)" ) {
-                    it.value = Float.parseFloat(it.value) / 1.609344
-                }
                 sendEvent(it)
                 break
             default:
--- hubitat-teslamate.groovy
+++ hubitat-teslamate.groovy
@@ -6,6 +6,7 @@
         - MQTT Link Driver by jeubanks, https://github.com/mydevbox/hubitat-mqtt-link
         - Hubitat Parent/Child Examples by MikeMaxwell, https://github.com/hubitat/HubitatPublic/
 */
+import groovy.transform.Field
 
 metadata {
     definition (name: "TeslaMate", namespace: "evequefou", author: "Mike Bishop") {
@@ -19,52 +20,129 @@ metadata {
 
     }
     preferences {
-        input(
-            name: "brokerIp",
-            type: "string",
-            title: "MQTT Broker IP Address",
-            description: "e.g. 192.168.1.200",
-            required: true,
-            displayDuringSetup: true
-        )
-        input(
-            name: "brokerPort",
-            type: "string",
-            title: "MQTT Broker Port",
-            description: "e.g. 1883",
-            required: true,
-            displayDuringSetup: true
-        )
-        input(
-            name: "brokerUser",
-            type: "string",
-            title: "MQTT Broker Username",
-            description: "e.g. mqtt_user",
-            required: false,
-            displayDuringSetup: true
-        )
-        input(
-            name: "brokerPassword",
-            type: "password",
-            title: "MQTT Broker Password",
-            description: "e.g. ^L85er1Z7g&%2En!",
-            required: false,
-            displayDuringSetup: true
-        )
+        section("MQTT Configuration") {
+            input(
+                name: "brokerIp",
+                type: "string",
+                title: "MQTT Broker IP Address",
+                description: "e.g. 192.168.1.200",
+                required: true,
+                displayDuringSetup: true
+            )
+            input(
+                name: "brokerPort",
+                type: "string",
+                title: "MQTT Broker Port",
+                description: "e.g. 1883",
+                required: true,
+                displayDuringSetup: true
+            )
+            input(
+                name: "brokerUser",
+                type: "string",
+                title: "MQTT Broker Username",
+                description: "e.g. mqtt_user",
+                required: false,
+                displayDuringSetup: true
+            )
+            input(
+                name: "brokerPassword",
+                type: "password",
+                title: "MQTT Broker Password",
+                description: "e.g. ^L85er1Z7g&%2En!",
+                required: false,
+                displayDuringSetup: true
+            )
+            input(
+                name: "namespace",
+                type: "string",
+                title: "TeslaMate custom namespace",
+                description: "Typically used for multiple TeslaMate instances",
+                required: false,
+                displayDuringSetup: true
+            )
+        }
+        section("Data Processing") {
+            input(
+                name: "homeGeofence",
+                type: "string",
+                title: "Geofence for Presence",
+                required: false,
+                defaultValue: "Home"
+            )
+            input(
+                name: "showPower",
+                type: "bool",
+                title: "Expose power and charging details",
+                required: false,
+                default: false
+            )
+            input(
+                name: "showClimate",
+                type: "bool",
+                title: "Expose climate and temperature details",
+                required: false,
+                default: false
+            )
+            input(
+                name: "showSecurity",
+                type: "bool",
+                title: "Expose security and door state",
+                required: false,
+                default: false
+            )
+            input(
+                name: "showCoarseDriving",
+                type: "bool",
+                title: "Expose low-frequency driving details",
+                required: false,
+                default: false
+            )
+            if( showCoarseDriving ) {
+                input(
+                    name: "showDetailedDriving",
+                    type: "bool",
+                    title: "Expose all driving details and precise location",
+                    description: "This is a really bad idea",
+                    required: false,
+                    default: false
+                )
+            }
+            if( showClimate ) {
+                input(
+                    name: "temperatureIndoor",
+                    type: "enum",
+                    title: "Temperature sensor reflects Indoor or Outdoor",
+                    required: false,
+                    defaultValue: "Outdoor",
+                    options: ["Indoor", "Outdoor"]
+                )
+                input(
+                    name: "tempFormat",
+                    type: "enum",
+                    required: true,
+                    defaultValue: "Fahrenheit (°F)",
+                    title: "Display Unit - Temperature: Fahrenheit (°F) or Celsius (°C)",
+                    options: ["Fahrenheit (°F)", "Celsius (°C)"]
+                )
+            }
+            if( showCoarseDriving || showDetailedDriving ) {
+                input(
+                    name: "rangeFormat",
+                    type: "enum",
+                    required: true,
+                    defaultValue: "miles (mi)",
+                    title: "Display Unit - Range: miles or kilometers",
+                    options: ["miles (mi)", "kilometers (ki)"]
+                )
+            }
+        }
         input(
             name: "debugLogging",
             type: "bool",
             title: "Enable debug logging",
             required: false,
             default: false
-        )
-        input(
-            name: "namespace",
-            type: "string",
-            title: "TeslaMate custom namespace",
-            description: "Typically used for multiple TeslaMate instances",
-            required: false,
-            displayDuringSetup: true
         )
     }
 }
@@ -81,8 +159,25 @@ void initialize() {
         // delay for connection
         pauseExecution(1000)
 
-        debug("[d:subscribe] full topic: ${getTopicPrefix()}#")
-        interfaces.mqtt.subscribe("${getTopicPrefix()}#")
+        def subscriptions = AlwaysAvailable
+        [
+            showPower: PowerDetails,
+            showClimate: ClimateDetails,
+            showSecurity: SecurityDetails,
+            showCoarseDriving: CoarseDriving,
+            showDetailedDriving: BadIdeas
+         ].each {
+             setting, topics ->
+                if( settings[setting] ) {
+                    subscriptions += topics
+                }
+         }
+
+        for( topic in subscriptions ) {
+            def fullTopic = "${getTopicPrefix()}+/${topic}"
+            debug("[d:subscribe] full topic: ${fullTopic}")
+            interfaces.mqtt.subscribe(fullTopic)
+        }
         connected()
     } catch(Exception e) {
         error("[d:initialize] ${e}")
@@ -126,6 +221,7 @@ def parse(String event) {
     debug("[d:parse] Received MQTT message: ${message}")
 
     def (id, property) = message.topic.minus(getTopicPrefix()).tokenize('/')
+    def value = message.payload
 
     def thisId = device.deviceNetworkId
     def cd = getChildDevice("${thisId}-${id}")
@@ -139,7 +235,50 @@ def parse(String event) {
         cd.setLabel(message.payload)
     }
     else {
-        cd.parse([[name:property, value:message.payload]])
+        if( Transforms[property] ) {
+            property = Transforms[property]
+        }
+
+        def toProcess = []
+        switch(property) {
+            case "geofence":
+                toProcess.add([
+                    "name": "presence",
+                    "value": value == settings?.homeGeofence ? "present" : "not present"
+                ])
+                break
+
+            case "inside_temp":
+            case "outside_temp":
+                value = Float.parseFloat(value)
+                if( settings?.tempFormat == "Fahrenheit (°F)" ) {
+                    // Convert temperature formats
+                    value = celsiusToFahrenheit(value)
+                }
+                if( settings?.temperatureIndoor ^ property == "outside_temp" ) {
+                    toProcess.add([
+                        "name": "temperature",
+                        "value": value
+                    ])
+                }
+                break
+
+            case "lock":
+                value = value == "true" ? "locked" : "unlocked"
+                break
+
+            case "est_battery_range":
+            case "rated_battery_range":
+            case "ideal_battery_range":
+            case "odometer":
+                if( settings?.rangeFormat == "miles (mi)" ) {
+                    value = Float.parseFloat(value) / 1.609344
+                }
+                break
+        }
+        toProcess.add([name: property, value: value])
+
+        cd.parse(toProcess)
     }
 }
 
@@ -210,3 +349,83 @@ def warn(msg) {
 def error(msg) {
     log.error msg
 }
+
+@Field static final List AlwaysAvailable = [
+    "display_name",
+    "geofence",
+    "battery_level",
+    "plugged_in",
+    "state",
+    "since",
+    "healthy",
+    "version",
+    "update_available",
+    "update_version",
+    "scheduled_charging_start_time",
+    "time_to_full_charge"
+]
+
+// Power and Charging Details
+@Field static final List PowerDetails = [
+    "charger_voltage",
+    "charger_actual_current",
+    "charger_phases",
+    "est_battery_range",
+    "rated_battery_range",
+    "ideal_battery_range",
+    "usable_battery_level",
+    "charge_energy_added",
+    "charge_limit_soc",
+    "charge_port_door_open",
+    "charge_current_request",
+    "charge_current_request_max"
+]
+
+// Temperature
+@Field static final List ClimateDetails = [
+    "is_climate_on",
+    "inside_temp",
+    "outside_temp",
+    "is_preconditioning"
+]
+
+// Security
+@Field static final List SecurityDetails = [
+    "windows_open",
+    "doors_open",
+    "frunk_open",
+    "trunk_open",
+    "locked",
+    "sentry_mode"
+]
+
+// Coarse Location & Driving
+@Field static final List CoarseDriving = [
+    "shift_state",
+    "odometer"
+]
+
+// Detailed Location & Driving
+// WARNING:  These are exceptionally chatty;
+//    do not enable unless critical!
+// What you want to do can probably be done
+// by defining a geofence in TeslaMate instead.
+@Field static final List BadIdeas = [
+    "latitude",
+    "longitude",
+    "speed",
+    "heading",
+    "elevation"
+]
+
+@Field final Map Transforms = [
+    battery_level: "battery",
+    charger_voltage: "voltage",
+    charger_actual_current: "current",
+    charger_phases: "phases",
+    locked: "lock",
+    est_battery_range_km: "est_battery_range",
+    rated_battery_range_km: "rated_battery_range",
+    ideal_battery_range_km: "ideal_battery_range"
+]
+
